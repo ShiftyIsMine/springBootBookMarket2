0d3c90310d15848497191585b3b68f3b55772f85
Shifty <162399486+ShiftyIsMine@users.noreply.github.com>
2025-06-18 11:33:08 +0900

    0618

---
+++ src/main/java/kr/ac/kopo/minn/bookmarket/repository/CartRepository.java
@@ -0,0 +1,11 @@
+package kr.ac.kopo.minn.bookmarket.repository;
+
+import kr.ac.kopo.minn.bookmarket.domain.Cart;
+
+public interface CartRepository {
+    Cart create(Cart cart);
+
+    Cart read(String cartId);
+
+    void update(String cartId, Cart cart);
+}
